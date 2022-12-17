4b14244067a90382355d34b0b0095f11dba805ef
Joaquín Corugedo <joaquin_coru@hotmail.com>
2022-12-17 16:48:01 +0100

    Añadidas dependencias y API_KEY Google Maps con fragment de prueba

--- app/src/main/java/com/joaquinco/androidavanzado/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/joaquinco/androidavanzado/ui/detail/DetailFragment.kt
@@ -10,6 +10,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.joaquinco.androidavanzado.R
 import com.joaquinco.androidavanzado.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,7 +24,7 @@ import dagger.hilt.android.AndroidEntryPoint
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 @AndroidEntryPoint
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment() , OnMapReadyCallback{
 
     private var _binding: FragmentDetailBinding? = null
 
@@ -28,6 +34,8 @@ class DetailFragment : Fragment() {
 
     private val args:DetailFragmentArgs by navArgs()
     private val viewModel: DetailViewModel by viewModels()
+
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,10 +65,36 @@ class DetailFragment : Fragment() {
 
 
         viewModel.getHeroDetail(args.heroName)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+
+        //Listeners que permiten poder moverse dentro del mapa estando dentro del Scroll View
+        googleMap.setOnCameraMoveStartedListener {
+            mapFragment?.view?.parent?.requestDisallowInterceptTouchEvent(true)
+        }
+        googleMap.setOnCameraIdleListener {
+            mapFragment?.view?.parent?.requestDisallowInterceptTouchEvent(false)
+        }
+
+        googleMap.uiSettings.isZoomControlsEnabled= true
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 0.5f))
     }
 }
